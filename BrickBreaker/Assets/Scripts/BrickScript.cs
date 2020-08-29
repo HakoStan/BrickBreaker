12bbb65c6971b1ac8339fb1f96131ffd46db1669
efim <efim@ashkinazi.co.il>
2020-08-29 18:08:16 +0300

    Adding Bricks and bricks Logic

---
+++ BrickBreaker/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    public int hitCounter = 1;
+
+    public void RecieveHitCount(int hitCounter)
+    {
+        this.hitCounter = hitCounter;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "Ball")
+            return;
+        
+        hitCounter--;
+        if (hitCounter == 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
